52420ba3bac0013b44ddfecb5f3364ea958434dc
Bathacker <diegorscorpion@gmail.com>
2016-08-19 22:06:20 -0500

    Clase 1 y Clase 2
    Conocimientos visto en clase de programación web

---
+++ practicas comandos simbolo del sistema/script.bat
@@ -0,0 +1,5 @@
+@echo "Hola soy un script chafa por que estoy en windows :P"
+@echo "Eliminare la carpeta borrar, dar enter para continuar"
+pause
+rmdir /s \Users\UnMalNombre\Desktop\borrar
+@echo "Se borro carpeta, bye bye"
